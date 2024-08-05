cb42642b428eb11cb8e67a63ae8289cccfb43be4
Hyeonseob Lee <97226053+LeeHyeonseob@users.noreply.github.com>
2024-08-05 14:33:12 +0900

    feat: 연속 접속 check 추가

--- HealthBoard/src/main/java/org/health/repository/UserRepository.java
+++ HealthBoard/src/main/java/org/health/repository/UserRepository.java
@@ -1,7 +1,10 @@
 package org.health.repository;
 
 import org.health.domain.UserDTO;
+import org.health.domain.UserLoginInfoDTO;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface UserRepository {
@@ -9,4 +12,10 @@ public interface UserRepository {
     int login(String user);
     void logout();
     List<UserDTO> findAll();
+    UserLoginInfoDTO findUserIdAndDate(int userId);
+    int updateLoginDate(int userId, LocalDateTime date);
+
+    boolean isConsecutive(int userId);
+
+    boolean isConsecutiveForTest(int userId);
 }
