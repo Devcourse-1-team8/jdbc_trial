a38e8dcf9ed250e2409174be4e43e9a2d829ada4
ECON <lgyoon621@naver.com>
2024-08-05 14:33:53 +0900

    feat: 좋아요 추가, 삭제 기능 구현

--- HealthBoard/src/main/java/org/health/repository/DBUtil.java
+++ HealthBoard/src/main/java/org/health/repository/DBUtil.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class DBUtil {
-    public static final String url = "jdbc:mariadb://svc.sel5.cloudtype.app:30739/workshop";
+    public static final String url = "jdbc:mariadb://svc.sel5.cloudtype.app:30739/healthboard";
     public static final String user = "root";
     public static final String pwd = "1234";
 
