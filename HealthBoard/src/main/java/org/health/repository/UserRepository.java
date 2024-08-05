e62366a4dc91e1f2685f4197fe94fff01ffee471
TrixieL20 <minjung0768@gmail.com>
2024-08-05 15:08:13 +0900

    fix : add throws SQLException

--- HealthBoard/src/main/java/org/health/repository/UserRepository.java
+++ HealthBoard/src/main/java/org/health/repository/UserRepository.java
@@ -8,5 +8,5 @@ public interface UserRepository {
     int addUser(UserDTO user);
     int login(String user);
     void logout();
-    List<UserDTO> findAll();
+    List<UserDTO> findAll() throws SQLException;
 }
